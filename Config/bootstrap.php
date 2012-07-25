d8cf594dbf6a8c14f5ea874c7bac3f02bc1ccc56
Pierre Criulanscy <pcriulan@gmail.com>
2012-07-25 18:10:34 +0200

    Add ZendGdata-1.11.12 libraries and include_path

---
+++ Config/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+$ZendLibrary = App::pluginPath('YouTube') . DS . 'Vendor' . DS . 'ZendGdata-1.11.12' . DS . 'library';
+set_include_path(get_include_path() . PATH_SEPARATOR . $ZendLibrary);
+?>
